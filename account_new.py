1a9d1e351ef8c5852ae8d2012e0d0502b85ba839
hetvi-21 <heth.silentinfotech@gmail.com>
2025-09-25 16:53:08 +0530

    Update account_new file

--- account_new.py
+++ account_new.py
@@ -2,5 +2,8 @@ from odoo import models, fields
 
 class SaleOrder(models.Model):
     _inherit='account.move'
+    description = "inherit account.move to add custom fields"
 
     custom_field = fields.Char(string="Custom Field")
+    name =  fields.char(stirng ="customer name")
+
